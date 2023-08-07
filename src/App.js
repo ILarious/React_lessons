59d1b7b85ad2e8e7ebe28feb9ba5f4d143a89ce5
Ilya Badaev <ilarious08@yandex.ru>
2023-08-07 19:09:31 +0300

    add modal animated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import './index.scss';
 
-// Вариант испольнения модального окна через условный рендер
+
+const Modal = ({open, setOpen, children}) => (
+    <div className={`overlay animated ${open ? 'show' : ''}`}>
+        <div className="modal">
+            <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
+                <title/>
+                <path
+                    d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z"/>
+            </svg>
+            {children}
+        </div>
+    </div>
+)
 
 function App() {
     const [open, setOpen] = React.useState(false)
@@ -9,21 +21,18 @@ function App() {
 
     return (
         <div className="App">
-            <button onClick={() => setOpen(true)} className="open-modal-btn">✨ Открыть окно</button>
-            {open && (
-                <div className="overlay">
-                    <div className="modal">
-                        <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
-                            <title/>
-                            <path
-                                d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z"/>
-                        </svg>
-                        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif"/>
-                    </div>
-                </div>)}
+            <button onClick={() => setOpen(true)} className="open-modal-btn">
+                ✨ Открыть окно
+            </button>
+            <Modal open={open} setOpen={setOpen}>
+                <iframe width="500" height="250" src="https://www.youtube.com/embed/J0X1yOYKAas"
+                        title="Уважаю каждого Человека паука.Ты меня в том числе?Нет сраный негр!" frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowFullScreen></iframe>
+                <h2>Это ключевое событие, бро</h2>
+            </Modal>
         </div>
     );
-
 }
 
 export default App;
