7b97a8620647dd1c3e1257ac0400a22835aba010
Ilya Badaev <ilarious08@yandex.ru>
2023-08-10 11:24:07 +0300

    add users and skeleton

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import './index.scss';
-import { Success } from './components/Success';
-import { Users } from './components/Users';
+import {Success} from './components/Success';
+import {Users} from './components/Users';
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
-  return (
-    <div className="App">
-      <Users />
-      {/* <Success /> */}
-    </div>
-  );
+    const [users, setUsers] = React.useState([]);
+    const [isLoading, setIsLoading] = React.useState(true);
+
+    React.useEffect(() => {
+        fetch('https://reqres.in/api/users')
+            .then((response) => response.json())
+            .then((json) => {
+                setUsers(json.data);
+            }).catch(error => {
+                console.warn(error);
+                alert('Ошибка при получении списка пользователей');
+            }).finally(() => {
+                setIsLoading(false);
+            })
+    }, []);
+
+    return (
+        <div className="App">
+            <Users items={users} isLoading={isLoading}/>
+            {/* <Success /> */}
+        </div>
+    );
 }
 
 export default App;
