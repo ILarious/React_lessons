940fd5451110f7e75a04daa3d0e5cbc29469a326
Ilya Badaev <ilarious08@yandex.ru>
2023-08-20 04:49:01 +0300

    add rates

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import {Block} from './Block';
 import './index.scss';
 
 function App() {
+    const [rates, setRates] = React.useState({});
+
+    React.useEffect(() => {
+        fetch('https://cdn.cur.su/api/latest.json')
+            .then((response) => response.json())
+            .then((json) => {
+                setRates(json.rates);
+            })
+            .catch((error) => {
+                console.warn(error);
+                alert('Не удалось получить информацию о валютах');
+            });
+    }, []);
+
     return (
         <div className="App">
             <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)}/>
